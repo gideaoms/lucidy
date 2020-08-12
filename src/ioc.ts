01916cc7438eacb03bd29ee0a402e40e6066a45d
Gideão Silva <gideaoms@gmail.com>
2020-08-12 20:42:22 -0300

    move providers to ioc.js

--- src/ioc.ts
+++ src/ioc.ts
@@ -1,5 +1,34 @@
-import { Ioc } from '@adonisjs/fold';
+import { Ioc, Registrar } from '@adonisjs/fold';
+import { Config } from '@adonisjs/config/build/src/Config';
+import { DATABASE_ROOT, APP_ROOT } from './constants';
 
 const ioc = new Ioc();
+const registrar = new Registrar(ioc);
+
+ioc.singleton('Adonis/Core/Config', () => {
+  const database = require(DATABASE_ROOT).default;
+  const app = {
+    logger: {
+      name: 'logger',
+      enabled: true,
+      level: 'info',
+    },
+  };
+  return new Config({ app, database });
+});
+
+ioc.singleton('Adonis/Core/Application', () => {
+  const namespacesMap = new Map();
+
+  return {
+    appRoot: APP_ROOT,
+    configPath: String,
+    namespacesMap,
+  };
+});
+
+registrar
+  .useProviders(['@adonisjs/profiler', '@adonisjs/logger', '@adonisjs/events', '@adonisjs/lucid'])
+  .register();
 
 export default ioc;
